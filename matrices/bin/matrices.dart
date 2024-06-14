64eb6cc1cbfbfac88226fc86012e84ff68f7b523
Alan Met <106497267+MysteryCrystal@users.noreply.github.com>
2024-06-14 05:32:57 +0100

    Initial upload

---
+++ matrices/bin/matrices.dart
@@ -0,0 +1,5 @@
+import 'package:matrices/matrices.dart' as matrices;
+
+void main(List<String> arguments) {
+  print('Hello world: ${matrices.calculate()}!');
+}
---
+++ matrices/test/matrices_test.dart
@@ -0,0 +1,8 @@
+import 'package:matrices/matrices.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
